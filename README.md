bf3b313e0cb509af4c13ea674ea3b5375813060b
Walleson Moura <phor3nsic@MacBook-Air-de-Walleson.local>
2021-08-12 13:41:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Shodan Toolkit
+
+> This tool aims to bring together some main forms of research by shodan and unite the ips, with each new form of interesting research a new module will be implemented.
+
+## Usage
+
+- Install:
+
+```
+git clone https://github.com/phor3nsic/shodan-toolkit.git
+cd shodan-toolkit
+pip3 -r requirements.txt
+```
+
+- Searchs:
+	- ssl
+	- favicon
+
+### Help
+```
+python3 st.py [-h] -k API_KEY [-d DOMAIN] [-u URL] [-ssl] [-favicon] [-ipv6]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -k API_KEY, --api_key API_KEY
+                        Api Key
+  -d DOMAIN, --domain DOMAIN
+                        Domain for search
+  -u URL, --url URL     Url
+  -ssl, --ssl           SSL Search
+  -favicon, --favicon   Favicon Mode default=False
+  -ipv6, --ipv6         IPV6 accepted
+```
+-- FAVICON
+
+```
+python3 st.py -u https://twitter.com/favicon.ico -k 96XABC123444CCADASDGG... -favicon
+
+27.120.102.97
+188.165.80.13
+101.37.170.108
+104.244.42.198
+...
+```
+
+-- SSL
+
+```
+python3 st.py -d twitter.com -k 96XABC123444CCADASDGG... -ssl
+
+192.229.237.25
+209.237.211.128
+209.237.193.139
+199.16.156.41
+104.244.42.199
+
+...
+```
---
+++ st.py
@@ -0,0 +1,65 @@
+import shodan
+import argparse
+import mmh3
+import requests
+import codecs
+
+def disable_warnings():
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+def shodan_search(queryes, api_key):
+    api = shodan.Shodan(api_key)
+    hosts = []
+    for x in queryes:
+        try:
+            result = api.search(x)
+            for services in result['matches']:
+                if services['ip_str'] not in hosts:
+                    if ":" not in services['ip_str']: # NOT GET IPV6
+                        hosts.append(services['ip_str'])
+                        print(services['ip_str'])
+        except:
+            pass
+
+def ssl_mode(domain, api_key):
+    queryes = ["ssl:"+domain, "ssl.cert.subject.CN:"+domain]
+    shodan_search(queryes, api_key)
+
+def favicon_mode(url_favicon, api_key):
+    disable_warnings()
+    resp = requests.get(url_favicon, verify=False)
+    favicon = codecs.encode(resp.content,"base64")
+    hash_favicon = mmh3.hash(favicon)
+    search_favicon = ["http.favicon.hash:"+str(hash_favicon)]
+    shodan_search(search_favicon, api_key)
+
+def main():
+
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument("-k","--api_key", help="Api Key", required=True)
+    parser.add_argument("-d", "--domain", help="Domain for search")
+    parser.add_argument("-u", "--url", help="Url")
+    parser.add_argument("-ssl","--ssl", help="SSL Search", action="store_true")
+    parser.add_argument("-favicon","--favicon", help="Favicon Mode default=False", action="store_true")
+    parser.add_argument("-ipv6", "--ipv6", help="IPV6 accepted", action="store_true")
+    args = parser.parse_args()
+    IPV6 = False
+    if args.ssl == True:
+        if args.domain:
+            ssl_mode(args.domain, args.api_key)
+        else:
+            print("[!] Error: it's necessary to use -d/--domain")
+
+    if args.favicon == True:
+        if args.url:
+            favicon_mode(args.url, args.api_key)
+        else:
+            print("[!] Error: it's necessary to use -u/--url")
+
+    if args.ipv6 == True:
+        IPV6 = True
+
+if __name__ == "__main__":
+    main()
+    
